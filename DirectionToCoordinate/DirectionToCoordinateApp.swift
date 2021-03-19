ca80f936a73f094f3061c5f93089504319acb04b
Dominik Hauser <dom@dasdom.de>
2021-03-19 09:16:58 +0100

    Initial Commit

---
+++ DirectionToCoordinate/DirectionToCoordinateApp.swift
@@ -0,0 +1,14 @@
+//  Created by Dominik Hauser on 19/03/2021.
+//  
+//
+
+import SwiftUI
+
+@main
+struct DirectionToCoordinateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
