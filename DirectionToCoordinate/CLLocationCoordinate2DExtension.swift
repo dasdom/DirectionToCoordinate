f0ec303a496e78bef6627deccfe4c36e1bc5a4f3
Dominik Hauser <dom@dasdom.de>
2021-03-21 10:42:16 +0100

    Add map to main app

--- DirectionToCoordinate/CLLocationCoordinate2DExtension.swift
+++ DirectionToCoordinate/CLLocationCoordinate2DExtension.swift
@@ -14,6 +14,7 @@ func rad2deg(_ number: Double) -> Double {
 
 extension CLLocationCoordinate2D {  
   
+  // https://stackoverflow.com/q/3925942/498796
   func bearing(to coordinate: CLLocationCoordinate2D) -> Double {
     
     let lat1 = deg2rad(latitude)
@@ -29,5 +30,24 @@ extension CLLocationCoordinate2D {
     let radiansBearing = atan2(y, x)
     
     return (rad2deg(radiansBearing) + 360).truncatingRemainder(dividingBy: 360)
+  }
+  
+  // https://stackoverflow.com/q/10559219/498796
+  func center(to coordinate: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
+    let lon1 = deg2rad(longitude)
+    let lon2 = deg2rad(coordinate.longitude)
+    
+    let lat1 = deg2rad(latitude)
+    let lat2 = deg2rad(coordinate.latitude)
+    
+    let dLon = lon2 - lon1
+    
+    let x = cos(lat2) * cos(dLon)
+    let y = cos(lat2) * sin(dLon)
+    
+    let lat3 = atan2( sin(lat1) + sin(lat2), sqrt((cos(lat1) + x) * (cos(lat1) + x) + y * y) )
+    let lon3 = lon1 + atan2(y, cos(lat1) + x)
+    
+    return CLLocationCoordinate2D(latitude: rad2deg(lat3), longitude: rad2deg(lon3))
   }
 }
--- DirectionToCoordinate/ContentView.swift
+++ DirectionToCoordinate/ContentView.swift
@@ -4,14 +4,17 @@
 
 import SwiftUI
 import CoreLocation
+import MapKit
 
 struct ContentView: View {
-
+  
   @State var address: String = ""
   @State var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D()
   @State var error: Error?
+  @State var region: MKCoordinateRegion = MKCoordinateRegion()
+  @State var annotationItems: [CLLocationCoordinate2D] = []
   @EnvironmentObject private var locationProvider: LocationProvider
-
+  
   var location: CLLocation {
     return CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
   }
@@ -24,52 +27,88 @@ struct ContentView: View {
   }
   var distance: Double {
     if let myLocation = locationProvider.location {
-      return myLocation.distance(from: location)
+      let distance = myLocation.distance(from: location)
+      return distance
     } else {
       return 0
     }
   }
-
+  
   var body: some View {
     VStack {
-      VStack {
-        TextField("Put in an address or a city.", text: $address, onCommit: {
-          CLGeocoder().geocodeAddressString(address) { placementMarks, error in
-            if let placementMark = placementMarks?.first, let location = placementMark.location {
-              coordinate = location.coordinate
-            }
-          }
-        })
-        .multilineTextAlignment(.center)
-        
-        if abs(coordinate.latitude) > 0.001, abs(coordinate.longitude) > 0.001 {        Text("\(coordinate.latitude)  \(coordinate.longitude)")
-          .font(.footnote)
-          .foregroundColor(Color(UIColor.secondaryLabel))
-        }
-      }
-      
-      VStack {
-        Image(systemName: "location.north.fill")
-          .resizable()
-          .aspectRatio(contentMode: .fit)
-          .rotationEffect(Angle(degrees: bearing - (locationProvider.heading?.magneticHeading ?? 0)))
-          .frame(height: 100)
-          .padding()
-        if let error = error {
-          Text("\(error.localizedDescription)")
-            .padding()
+      Map(coordinateRegion: $region, interactionModes: .all, annotationItems: annotationItems, annotationContent: { item -> MapMarker in
+        let color: Color?
+        if item.id == self.coordinate.id {
+          color = .red
         } else {
-          Text(String(format: "%.2lf km", distance/1000))
-            .font(.headline)
-            .padding()
+          color = .gray
         }
-      }
+        return MapMarker(coordinate: item, tint: color)
+      })
       
+      VStack {
+        VStack {
+          TextField("Put in an address or a city.", text: $address, onCommit: {
+            CLGeocoder().geocodeAddressString(address) { placementMarks, error in
+              if let placementMark = placementMarks?.first, let location = placementMark.location {
+                
+                coordinate = location.coordinate
+                
+                updateAnnotations()
+              }
+            }
+          })
+          .multilineTextAlignment(.center)
+          
+          if abs(coordinate.latitude) > 0.001, abs(coordinate.longitude) > 0.001 {
+            Text("\(coordinate.latitude)  \(coordinate.longitude)")
+              .font(.footnote)
+              .foregroundColor(Color(UIColor.secondaryLabel))
+          }
+        }
+        
+        VStack {
+          Image(systemName: "location.north.fill")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .rotationEffect(Angle(degrees: bearing - (locationProvider.heading?.magneticHeading ?? 0)))
+            .frame(height: 100)
+            .padding()
+          
+          if let error = error {
+            Text("\(error.localizedDescription)")
+              .padding()
+          } else {
+            Text(String(format: "%.2lf km", distance/1000))
+              .font(.headline)
+              .padding()
+          }
+        }
+        
+      }
+      .padding()
+      //      .background(Color(UIColor.systemBackground).opacity(0.7))
+      .onAppear(perform: {
+        locationProvider.start()
+      })
     }
-    .padding()
-    .onAppear(perform: {
-      locationProvider.start()
-    })
+  }
+  
+  func updateAnnotations() {
+    if let myCoordinate = locationProvider.location?.coordinate {
+      annotationItems = [coordinate]
+      annotationItems.append(myCoordinate)
+      
+      let mid = myCoordinate.center(to: coordinate)
+      region = MKCoordinateRegion(center: mid, latitudinalMeters: distance, longitudinalMeters: distance)
+    }
+  }
+}
+
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-show-annotations-in-a-map-view
+extension CLLocationCoordinate2D: Identifiable {
+  public var id: String {
+    "\(latitude)-\(longitude)"
   }
 }
 
