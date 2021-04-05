0b5f8797e2932425d2c3ccf978de27eea8a88a5c
Dominik Hauser <dom@dasdom.de>
2021-04-05 21:33:42 +0200

    Improve readability of the code

---
+++ Heada/LatLongText.swift
@@ -0,0 +1,17 @@
+//  Created by Dominik Hauser on 05/04/2021.
+//  Copyright © 2021 dasdom. All rights reserved.
+//
+
+import SwiftUI
+
+struct LatLongText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LatLongText_Previews: PreviewProvider {
+    static var previews: some View {
+        LatLongText()
+    }
+}
